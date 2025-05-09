a3eb002c237ab6891bb503ebc2073fca143bd71c
R4resDev <raresnecula05@gmail.com>
2025-05-09 12:59:27 +0300

    Update and rename game_exceptions.cpp to exceptions.cpp

--- exceptions.cpp
+++ exceptions.cpp
@@ -1,4 +1,4 @@
-#include "game_exceptions.h"
+#include "exceptions.h"
 
 const char* InvalidLevelException::what() const noexcept {
     return "Nivel invalid! Nivelul trebuie sa fie intre 1 si 5.";
@@ -8,8 +8,13 @@ const char* InvalidInputException::what() const noexcept {
     return "Input invalid! Introduceti doar 1 sau 0.";
 }
 
-GameOverException::GameOverException(int s) : score(s) {}
+const char* InvalidGameTypeException::what() const noexcept {
+    return "Tip de joc invalid! Alegeti intre 1, 2 sau 3.";
+}
 
+const char* InvalidChoiceException::what() const noexcept {
+    return "Optiune invalida! Alegeti 1 sau 2.";
+}
 const char* GameOverException::what() const noexcept {
-    return "Jocul s-a terminat! Scorul dumneavoastra este: ";
+    return "Joc terminat! Scor final: ";
 }
